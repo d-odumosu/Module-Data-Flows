3e0a0bbb6c0626eb24353baaa7902907562541f7
d-odumosu <dt.odumosu@gmail.com>
2026-08-04 23:23:05 +0100

    implemented the function to print out a receipt

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,15 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+let receipt = `${"QTY".padEnd(6)}${"ITEM".padEnd(20)}${"TOTAL"}`;
+let total = 0;
+function printOrder(orders) {
+  orders.forEach(({ itemName, quantity, unitPricePence }) => {
+    receipt += `\n${quantity.toString().padEnd(6)}${itemName.padEnd(20)}${(unitPricePence / 100).toFixed(2)}\n`;
+    total += unitPricePence;
+  });
+  receipt += `\nTotal: ${(total / 100).toFixed(2)} `;
+  return receipt;
+}
+console.log(printOrder(order));
